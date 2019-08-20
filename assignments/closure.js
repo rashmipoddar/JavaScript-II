252f290e6528c942ec3d14e6b9a73ee5888799cb
Rashmi <poddar.rashmi@gmail.com>
2019-08-20 17:46:02 -0400

    Completed basic requirements of closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -4,6 +4,16 @@
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
 
+let adder = (num) => {
+  return function(num2) {
+    return num + num2;
+  }
+}
+
+let add10 = adder(10);
+// console.log(add10);
+console.log(add10(5));
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
