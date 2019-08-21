ac35d1d9ca08c51919cfcf20a3a17e58d1fd69a2
Rashmi <poddar.rashmi@gmail.com>
2019-08-21 11:53:38 -0400

    Refactored callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -81,15 +81,16 @@ console.log(test4);
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-  for (let i = 0; i < list.length; i++) {
-    if (list[i] === item) {
-      return cb(true);
-    }
-  }
-  return cb(false);
+  // for (let i = 0; i < list.length; i++) {
+  //   if (list[i] === item) {
+  //     return cb(true);
+  //   }
+  // }
+  // return cb(false);
+  return cb(list.includes(item));
 }
 
-const test5 = contains('yo-yo', items, boolean => `The item is included in the list ${boolean}`);
+const test5 = contains('yoyo', items, boolean => `The item is included in the list ${boolean}`);
 console.log(test5);
 
 /* STRETCH PROBLEM */
@@ -98,14 +99,18 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
-  let uniqueArray = [];
-  for (let i = 0; i < array.length; i++) {
-    if(uniqueArray.indexOf(array[i]) === -1) {
-      uniqueArray.push(array[i]);
-    }
-  }
+  // let uniqueArray = [];
+  // for (let i = 0; i < array.length; i++) {
+  //   if(uniqueArray.indexOf(array[i]) === -1) {
+  //     uniqueArray.push(array[i]);
+  //   }
+  // }
+  // return cb(uniqueArray);
+  let uniqueArray = array.filter((item, index) => array.indexOf(item) === index);
   return cb(uniqueArray);
 }
 
-let test6 = removeDuplicates([1,2,4,1,3,2,5], array => array);
+let originalArray = [1,2,4,1,3,2,5];
+let test6 = removeDuplicates(originalArray, array => array);
+console.log(originalArray);
 console.log(test6);
